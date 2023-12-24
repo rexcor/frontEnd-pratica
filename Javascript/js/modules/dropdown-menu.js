a6d4e79c4ed43936483292dfb28a2350dd381fd7
Gabriel <gabrielmdoliveira@gmail.com>
2023-12-24 17:00:04 -0300

    Adicionado funcao de outsideclick

--- Javascript/js/modules/dropdown-menu.js
+++ Javascript/js/modules/dropdown-menu.js
@@ -1,13 +1,19 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll("[data-dropdown]");
 
   dropdownMenus.forEach((menu) => {
-    menu.addEventListener("click", handleClick);
-    menu.addEventListener("touchstart", handleClick);
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
   });
 
   function handleClick(event) {
     event.preventDefault();
-    this.classList.toggle("active");
+    this.classList.add("active");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("active");
+    });
   }
 }
