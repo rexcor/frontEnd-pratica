e1ec7b249e22a40d5eaf0bfff98f9fb7316d0eb0
Gabriel <gabrielmdoliveira@gmail.com>
2024-01-26 12:34:59 -0300

    Refactor do menu mobile para classe

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -5,7 +5,7 @@ import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 
@@ -37,6 +37,8 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu("[data-dropdown]");
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initAnimaNumeros();
 initFuncionamento();
