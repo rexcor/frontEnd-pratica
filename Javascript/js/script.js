cfddaa170ccfef6c230b2f5e89a9a44b0138973c
Gabriel <gabrielmdoliveira@gmail.com>
2024-01-26 13:06:02 -0300

    Refactor do funcionamento para classe

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -6,8 +6,8 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
+import Funcionamento from "./modules/funcionamento.js";
 import initAnimaNumeros from "./modules/anima-numeros.js";
-import initFuncionamento from "./modules/funcionamento.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -40,5 +40,7 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
+
 initAnimaNumeros();
-initFuncionamento();
